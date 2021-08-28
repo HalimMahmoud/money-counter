d73dbe7e57978738628f85266e889713614c4db5
HalimMahmoud <halimmahmoud50@yahoo.com>
2021-08-28 13:50:32 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "teller",
   plugins: [reactRefresh()],
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
-})
+});
