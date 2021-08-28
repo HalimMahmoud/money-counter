391d687f93b46109cbf4589fd3eee2e2aed843e9
HalimMahmoud <halimmahmoud50@yahoo.com>
2021-08-28 13:52:04 +0200

    third commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "teller",
+  base: "/teller/",
   plugins: [reactRefresh()],
   esbuild: {
     jsxInject: `import React from 'react'`,
